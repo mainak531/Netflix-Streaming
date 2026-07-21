07f3b71808ce82edfd394d9a66542d416a002ab2
Mainak Chanda <mainakchanda531@gmail.com>
2026-07-21 18:39:57 +0530

    Microservices Created

---
+++ content-service/src/main/java/com/netflix/contentservice/model/Genre.java
@@ -0,0 +1,13 @@
+package com.netflix.contentservice.model;
+
+public enum Genre {
+    ACTION,
+    COMEDY,
+    DRAMA,
+    HORROR,
+    ROMANCE,
+    THRILLER,
+    DOCUMENTARY,
+    ANIMATION,
+    SCI_FI
+}
